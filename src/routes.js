4e330523ba05b67770bd6e454f83f666d502d976
phoenixsim94 <phoenixsim94@gmail.com>
2022-11-15 02:55:36 +0900

    feat:added save-mockup-data endpoint

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const { saveMockupData } = require('./data-access/data-repository');
+
 router.use((req, res, next) => {
   next();
 });
@@ -9,6 +11,18 @@ router.get('/', (req, res) => {
   res.json({
     message: 'Home',
   });
+});
+
+router.post('/save-mockup-data', async (req, res) => {
+  const reqData = req?.body;
+  if (reqData) {
+    const isSavedData = await saveMockupData(reqData);
+    if (isSavedData) {
+      res.status(200).send('Success');
+    } else {
+      res.status(503).send('Error has occurred during saving data in database');
+    }
+  }
 });
 
 module.exports = router;
