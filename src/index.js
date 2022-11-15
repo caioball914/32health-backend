ceb223a5346c6d15b728dec550e9ba6bc8643e38
crisps0914 <crisps0914@gmail.com>
2022-11-15 23:06:19 +0900

    refactor:removed unused code

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ require('dotenv').config();
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
 const cors = require('cors');
+const app = express();
 const admin = require('firebase-admin');
 const serviceAccount = require('../src/common/health-b7908-firebase-adminsdk-crbda-02e7537e0d.json');
 
-app.use(express.urlencoded({
-    extended: true
-}))
 app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,7 +15,6 @@ app.use(bodyParser.json());
 const port = process.env.APP_PORT || 8080;
 const host = process.env.APP_HOST || '127.0.0.1';
 
-// initializing firebase app
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: process.env.FIREBASE_DATABASE_URL
